5a12a330fb672bddcc03645228f4c8d83a4a20c9
Pandemic <pandemicjunk@gmail.com>
2021-09-09 17:07:29 -0400

    .eslintrc.js version 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,41 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+    'env': {
+        'browser': true,
+        'es6': true
     },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module"
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'sourceType': 'module'
     },
-    "rules": {
-        "indent": [
-            "error",
+    'rules': {
+        'indent': [
+            'error',
             4
         ],
-        "linebreak-style": [
-            "error",
-            "unix"
+        'linebreak-style': [
+            'error',
+            'unix'
         ],
-        "quotes": [
-            "error",
-            "single"
+        'quotes': [
+            'error',
+            'single'
         ],
-        "semi": [
-            "error",
-            "always"
+        'semi': [
+            'error',
+            'always'
         ],
-        "func-call-spacing": "error",
-        "func-name-matching": "error",
-        "func-names": "error",
-        "func-style": "error",
-        "function-paren-newline": "error",
-        "generator-star-spacing": "error",
-        "no-bitwise": "error",
-        "no-confusing-arrow": ["error", {"allowParens": true}],
-        "no-duplicate-imports": "error",
-        "space-in-parens": ["error", "never"],
-        "no-unused-vars": ["error", { "vars": "all", "args": "none", "ignoreRestSiblings": false }],
-        "camelcase": "error"
+        'func-call-spacing': 'error',
+        'func-name-matching': 'error',
+        'func-names': 'error',
+        'func-style': 'error',
+        'function-paren-newline': ['error', 'consistent'],
+        'generator-star-spacing': 'error',
+        'no-bitwise': 'error',
+        'no-confusing-arrow': ['error', {'allowParens': true}],
+        'no-duplicate-imports': 'error',
+        'space-in-parens': ['error', 'never'],
+        'no-unused-vars': 'off',
+        'no-undef': 'off',
+        'camelcase': 'error'
     }
 };
